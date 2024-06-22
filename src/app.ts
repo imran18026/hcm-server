cbba3f958ece20d6deb64ba5e2b08e35ac0f6f64
Md Imran Hossain <mdimranhossain.ist@gmail.com>
2024-06-23 01:37:12 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Hospital Care Management Server..!");
+});
+
+export default app;
